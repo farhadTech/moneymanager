1b5c1fadda9488689b9fccfa1ee67d7b50929921
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-07-16 04:08:54 -0700

    init

---
+++ moneymanager/src/test/java/com/farhad/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhad.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
